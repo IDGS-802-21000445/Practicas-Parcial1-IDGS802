17df88a388466a60d41c1da6514813a4ceb3e7e8
EdwinY-a <yovanirivers08@gmail.com>
2024-02-12 19:52:03 -0600

    EYRR - 12 02 2024 - Examen (Resistencia)

--- forms.py
+++ forms.py
@@ -7,3 +7,15 @@ class distancia_form(Form):
     PuntoX2 = StringField("Punto X2")
     PuntoY2 = StringField("Punto Y2")
 
+
+class colores_Form(Form):
+    color1 = SelectField('Primera Banda', choices=[('negro', 'Negro'), ('marron', 'Marrón'), ('rojo', 'Rojo'), ('naranja', 'Naranja'), ('amarillo', 'Amarillo'), ('verde', 'Verde'), ('azul', 'Azul'), ('violeta', 'Violeta'), ('gris', 'Gris'), ('blanco', 'Blanco')])
+    color2 = SelectField('Segunda Banda', choices=[('negro', 'Negro'), ('marron', 'Marrón'), ('rojo', 'Rojo'), ('naranja', 'Naranja'), ('amarillo', 'Amarillo'), ('verde', 'Verde'), ('azul', 'Azul'), ('violeta', 'Violeta'), ('gris', 'Gris'), ('blanco', 'Blanco')])
+    color3 = SelectField('Tercera Banda', choices=[('negro', 'Negro'), ('marron', 'Marrón'), ('rojo', 'Rojo'), ('naranja', 'Naranja'), ('amarillo', 'Amarillo'), ('verde', 'Verde'), ('azul', 'Azul'), ('violeta', 'Violeta'), ('gris', 'Gris'), ('blanco', 'Blanco')])
+
+    color4 = RadioField('Tolerancia', choices=[
+        ('dorado', 'Dorado'),
+        ('plateado', 'Plateado')
+    ])
+        
+
--- main.py
+++ main.py
@@ -23,6 +23,74 @@ def distancia():
         print(resultado)
     return render_template("distanciaDosPuntos.html", form=dis_puntos, resultado = resultado)
 
+@app.route("/colores", methods=["GET","POST"])
+def colores():
+    valor_negativo = 1
+    valor_positivo = 1
+    valor_resistencia1 = valor_resistencia2 = valor_resistencia3 = ""
+    colorHex_1 = colorHex_2 = colorHex_3 = colorHex_4 = ""
+    valor = 1 
+    colores_Form  =forms.colores_Form(request.form)
+    if request.method == 'POST':
+        color1= colores_Form.color1.data
+        color2= colores_Form.color2.data
+        color3= colores_Form.color3.data
+        color4= colores_Form.color4.data
+        print(color1,color2,color3,color4)
+
+        banda1 = {"negro":0,"marron":1,"rojo" :2,"naranja":3,"amarillo":4,"verde":5,"azul":6,"violeta":7,"gris":8,"blanco":9}
+        banda2 = {"negro":0,"marron":1,"rojo" :2,"naranja":3,"amarillo":4,"verde":5,"azul":6,"violeta":7,"gris":8,"blanco":9}
+        banda3 = {"negro": 1, "marron": 10, "rojo": 100, "naranja": 1000, "amarillo": 10000, "verde": 100000, "azul": 1000000, "violeta": 100000000, "gris": 1000000000, "blanco": 10000000000}
+        colores_hex = {"negro": "#000000", "marron": "#800000", "rojo": "#FF0000", "naranja": "#FFA500","amarillo": "#FFFF00", "verde": "#008000", "azul": "#0000FF", "violeta": "#EE82EE","gris": "#808080", "blanco": "#FFFFFF", "dorado": "#FFD700", "plateado": "#C0C0C0"}
+
+       
+
+        if color1 in banda1 and color1 in colores_hex:
+            valor_resistencia1 = banda1[color1]
+            colorHex_1 = colores_hex[color1]
+            print ("color Hexadecimal" ,colorHex_1)
+            print("color1 ", valor_resistencia1)
+        if color2 in banda2 and color2 in colores_hex:
+            valor_resistencia2 = banda2[color2]
+            colorHex_2 = colores_hex[color2]
+            print ("color Hexadecimal",colorHex_2)
+            print("color2: ", valor_resistencia2)
+        if color3 in banda3 and color3 in colores_hex:
+            valor_resistencia3 = banda3[color3]
+            colorHex_3 = colores_hex[color3]
+            print("color hexadecimal",colorHex_3)
+            print("color3", valor_resistencia3)    
+
+        valor_concatenado = str(valor_resistencia1) + str(valor_resistencia2)
+        print(valor_concatenado)
+        valor = int(valor_concatenado) * valor_resistencia3
+        print(valor)
+
+        
+
+        if color4 == "dorado" and color4 in colores_hex:
+            valor_doraCal = valor * 0.05
+            print(valor_doraCal)
+            valor_positivo = valor + valor_doraCal
+            print(valor_positivo)
+
+            valor_negativo = valor - valor_doraCal
+            print(valor_negativo)
+            colorHex_4 = colores_hex[color4]
+            print(colorHex_4)
+        elif color4 == "plateado":
+            valor_plataCal = valor * 0.10
+            print(valor_plataCal)
+            valor_positivo = valor + valor_plataCal
+            print(valor_positivo)
+
+            valor_negativo = valor - valor_plataCal
+            print(valor_negativo)
+            colorHex_4 = colores_hex[color4]
+            print(colorHex_4)
+
+        
+    return render_template("colores_Resistencia.html", form=colores_Form, valor_positivo=valor_positivo, valor_negativo=valor_negativo,valor = valor, colorHex_1=colorHex_1, colorHex_2=colorHex_2, colorHex_3=colorHex_3, colorHex_4=colorHex_4)
 
 @app.route("/resultado", methods=["POST"])
 def calcular():
@@ -48,5 +116,7 @@ def calcular():
         
         return render_template("formulario1.html", resultado=resultado)
 
+
+    
 if __name__ == "__main__":
     app.run(debug=True)
